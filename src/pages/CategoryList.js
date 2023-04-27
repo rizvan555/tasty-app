d29056d205cbdf7c1339d3087afeae489938914c
RizvanKarimov <karimovrizvan84@gmail.com>
2023-04-27 11:52:46 +0200

    structur is ready

---
+++ src/pages/CategoryList.js
@@ -0,0 +1,9 @@
+const CategoryList = () => {
+  return (
+    <div>
+      <h1>CategoryList</h1>
+    </div>
+  );
+};
+
+export default CategoryList;
---
+++ src/pages/ProductItem.js
@@ -0,0 +1,9 @@
+const ProductItem = () => {
+  return (
+    <div>
+      <h1>ProductItem</h1>
+    </div>
+  );
+};
+
+export default ProductItem;
