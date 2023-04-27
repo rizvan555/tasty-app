639a220a21446cea59179fb42712e938170ccfda
BaronForero <kata.baron.f@gmail.com>
2023-04-27 14:37:52 +0200

    update Random

--- src/components/Random.js
+++ src/components/Random.js
@@ -1,7 +1,20 @@
+import { useEffect, useState } from "react";
+
 const Random = () => {
+
+  const [randomMeals, setRandomMeals] = useState([])
+
+  useEffect(() => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+      .then((res) => res.json())
+      .then((data) =>
+        
+        setRandomMeals(data))
+  })
+
   return (
     <div>
-      <h1>Random</h1>
+      <h1>Random</h1><h1>Random</h1>
     </div>
   );
 };
