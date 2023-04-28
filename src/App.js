7a61634e6cdafa97c13a4cf6317a45151826cc98
RizvanKarimov <karimovrizvan84@gmail.com>
2023-04-28 09:59:12 +0200

    added new folders

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import CategoryList from "./pages/CategoryList";
 import Random from "./components/Random";
 import ProductDetails from "./pages/ProductDetails";
 import ProductItems from "./pages/ProductItems";
-
-
+import ProductList from "./pages/ProductList";
 
 function App() {
   return (
@@ -16,9 +15,8 @@ function App() {
         <Route path="/productItems" element={<ProductItems />} />
         <Route path="/productList" element={<ProductList />} />
         <Route path="/productList/:id" element={<ProductDetails />} />
-
+        <Route path="/categoryList" element={<CategoryList />} />
       </Routes>
-   <CategoryResults/>
     </div>
   );
 }
